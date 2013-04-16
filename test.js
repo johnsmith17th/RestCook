ba0862d7b3586b06cefd1b8a5480aa84fc82eda2
John Smith <john.smith.17th@gmail.com>
2013-04-16 16:35:59 +0800

    To setup basic info of resource

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-var RestCook = require('./index');
+var RestCook = require('./lib');
 
 var app = new RestCook();
+
+app.resource('test', {
+	name: 'Test',
+	//desc: 'Description of resource Test',
+	route: '/Mytest',
+});
+
+app.db('mongodb://localhost/test');
+
+console.log(app._resources);
+console.log(app._db);
