97e444b9b16d2ac0c6fc8c37b205a8c33d1ec0a0
John Smith <john.smith.17th@gmail.com>
2013-04-17 00:00:27 +0800

    To build application

--- test.js
+++ test.js
@@ -3,12 +3,26 @@ var RestCook = require('./lib');
 var app = new RestCook();
 
 app.resource('test', {
-	name: 'Test',
+	//name: 'Test',
 	//desc: 'Description of resource Test',
-	route: '/Mytest',
+	//route: '/test-all',
+	//model: 'TestModel',
+	//collection: 'testz',
+	schema: {
+		key: String,
+		value: String
+	},
+	pk: 'key',
+	action: {
+		index: {},
+		post: {},
+		get: {},
+		put: {},
+		del: {}
+	}
 });
 
 app.db('mongodb://localhost/test');
 
-console.log(app._resources);
+console.log(JSON.stringify(app._resources, null, 2));
 console.log(app._db);
